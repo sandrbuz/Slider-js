0a5f1fd177979a152caa8fdab700abf7694dcfbd
sandrbuz <sandrbuz@mail.ru>
2021-01-21 17:40:48 +0300

    Advanced js -slider

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
+var slider1 = {
+    imagesUrls: [],
+    currentImageIndex: 0,
+    showPrevBtn: document.getElementById('show-prev-btn'),
+    showNextBtn: document.getElementById('show-next-btn'),
+    slideImage: document.getElementById('slide-img'),
 
-// find items
-var showPrevBtn = document.getElementById('show-prev-btn');
-var showNextBtn = document.getElementById('show-next-btn');
-var slideImage = document.getElementById('slide-img');
+    start: function () {
+        // subscribe to events
+        this.showPrevBtn.addEventListener('click', this.onShowPrevBtnClick);
+        this.showNextBtn.addEventListener('click', this.onShowNextBtnClick);
 
-// subscribe to events
-showPrevBtn.addEventListener('click', onShowPrevBtnClick);
-showNextBtn.addEventListener('click', onShowNextBtnClick);
-
-// create images Array
-var imagesUrls = [];
-
-imagesUrls.push('https://vybratpravilno.ru/wp-content/uploads/2016/04/2.Ferrari-599XX-1024x768.jpg');
-imagesUrls.push('https://avatars.mds.yandex.net/get-zen_doc/1862846/pub_5eef2b77e24705067fe965d0_5eef2bca1fc1653038a9d853/scale_1200');
-imagesUrls.push('https://7960777a-2fd1-4b07-8bbb-896e98c4659c.selcdn.net/upload/prod_add2/096/product-291096-2.jpg');
-imagesUrls.push('https://img2.goodfon.ru/original/2880x1800/6/9b/bugatti-veyron-sport-car-avto.jpg');
-
-
-var currentImageIndex = 0;
-
-slideImage.src = imagesUrls[currentImageIndex];
-showPrevBtn.disabled = true;
+        // create images Array
+        this.imagesUrls.push('https://vybratpravilno.ru/wp-content/uploads/2016/04/2.Ferrari-599XX-1024x768.jpg');
+        this.imagesUrls.push('https://avatars.mds.yandex.net/get-zen_doc/1862846/pub_5eef2b77e24705067fe965d0_5eef2bca1fc1653038a9d853/scale_1200');
+        this.imagesUrls.push('https://7960777a-2fd1-4b07-8bbb-896e98c4659c.selcdn.net/upload/prod_add2/096/product-291096-2.jpg');
+        this.imagesUrls.push('https://img2.goodfon.ru/original/2880x1800/6/9b/bugatti-veyron-sport-car-avto.jpg');
 
 
 
 
+        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+        this.showPrevBtn.disabled = true;
+    },
+
+    onShowPrevBtnClick: function (e) {
+        this.currentImageIndex--;
+        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+        this.showNextBtn.disabled = false;
+
+
+        // disable prev button if need
+        if (this.currentImageIndex === 0) {
+            showPrevBtn.disabled = true;
+        }
+    },
 
 
 
-// functions on btns onShowNextBtnClick
-function onShowPrevBtnClick() {
-    currentImageIndex--;
-    slideImage.src = imagesUrls[currentImageIndex];
-    showNextBtn.disabled = false;
+    onShowNextBtnClick: function (e) {
+        this.currentImageIndex++;
+        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+        this.showPrevBtn.disabled = false;
 
 
-    // disable prev button if need
-    if (currentImageIndex === 0) {
-        showPrevBtn.disabled = true;
-    }
-}
+        // disable next button if need
+        if (this.currentImageIndex === (this.imagesUrls.length - 1)) {
+            this.showNextBtn.disabled = true;
+        }
+    },
 
 
 
-function onShowNextBtnClick() {
-    currentImageIndex++;
-    slideImage.src = imagesUrls[currentImageIndex];
-    showPrevBtn.disabled = false;
 
-
-    // disable next button if need
-    if (currentImageIndex === (imagesUrls.length - 1)) {
-        showNextBtn.disabled = true;
-    }
-}
+};
